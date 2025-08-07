813d6a249bf18893d09dd807d2af86c2de3a5073
junaid-13 <zufishanjunaid.1999@gmail.com>
2025-08-07 11:13:27 +0530

    Resolved the repository description error on jenkins branch.

--- GitHub_repository/main.tf
+++ GitHub_repository/main.tf
@@ -4,10 +4,8 @@ locals {
 
 resource "github_repository" "repo_name" {
   name                      = local.repository_name
-  description               = <<EOF
-                        This is a repository in which we will write the code for the end-to-end project.
-                        This repository will be used for both development and staging environments.
-                    EOF
+  description               = " This is a repository in which we will write the code for the end-to-end project. This repository will be used for both development and staging environments."
+
   visibility                = "public"
   has_issues                = true
   allow_merge_commit        = true
