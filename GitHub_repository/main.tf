92586b43879e7731f4df8e60e92ce8c9a1f881e3
junaid-13 <zufishanjunaid.1999@gmail.com>
2025-08-12 16:08:12 +0530

    Modified the main file.
    Make the repository visibility to public.

--- GitHub_repository/main.tf
+++ GitHub_repository/main.tf
@@ -6,7 +6,7 @@ locals {
 resource "github_repository" "repo_name" {
   name                      = local.repository_name
   description               = " This is a repository in which we will write the code for the end-to-end project. This repository will be used for both development and staging environments."
-  visibility                = "private"
+  visibility                = "public"
   has_issues                = true
   allow_merge_commit        = true
   allow_auto_merge          = true
