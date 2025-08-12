494e3c42513bc2c4363a5f628ef05fb0e377ad59
junaid-13 <zufishanjunaid.1999@gmail.com>
2025-08-12 15:29:02 +0530

    Modified main file in the github repository.
    Set the visibility to private.

--- GitHub_repository/main.tf
+++ GitHub_repository/main.tf
@@ -5,7 +5,7 @@ locals {
 resource "github_repository" "repo_name" {
   name                      = local.repository_name
   description               = " This is a repository in which we will write the code for the end-to-end project. This repository will be used for both development and staging environments."
-  visibility                = "public"
+  visibility                = "private"
   has_issues                = true
   allow_merge_commit        = true
   allow_auto_merge          = true
